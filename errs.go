f24b049b76a77ea3677fb1a734b779f8d54440f5
Branden J Brown <zephyrtronium@gmail.com>
2012-05-05 02:04:53 -0400

    added license (WTFPLv2) and disclaimers

--- errs.go
+++ errs.go
@@ -1,3 +1,8 @@
+// This is free software. It comes without any warranty, to the extent
+// permitted by applicable law. You can redistribute it and/or modify it under
+// the termos of the Do What the Fuck You Want To Public License, Version 2,
+// as published by Sam Hocevar. See COPYING for more details.
+
 package expr
 
 import "errors"
